689df533f2da0bff20e3d5e94b2c761df9f43244
choeyunseo <choeyunseo4060@naver.com>
2024-01-24 01:47:55 +0900

    Add selection sort

--- algorithm/selection_sort.py
+++ algorithm/selection_sort.py
@@ -4,8 +4,19 @@ def selection_sort(nums: List[int]) -> List[int]:
     """
     Selection sort on a list.
     """
-    pass
+    # 최솟값 찾기
+    for i in range(len(nums)):
+        min_index = i
+        for j in range(i, len(nums)):
+            if nums[i] > nums[j]:
+                min_index = j
+        # swap
+        temp = nums[i] 
+        nums[i] = nums[min_index]
+        nums[min_index] = temp     
 
+    return nums
 if __name__ == '__main__':
     # Write your test cases here
-    pass
+    testcode1 = [5, 4, 3, 2, 1]
+    assert selection_sort(testcode1) == [1, 2, 3, 4, 5]
