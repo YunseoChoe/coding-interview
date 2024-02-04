abc147e4d5b098d69971daa4bf9b301c9a113449
choeyunseo <choeyunseo4060@naver.com>
2024-02-05 00:17:19 +0900

    Update 2-6

--- coding_interview/2-6.py
+++ coding_interview/2-6.py
@@ -6,18 +6,25 @@ def is_palindrome(ll: LinkedList) -> bool:
     """
     Check if a linked list is a palindrome.
     """
-    # ll_list
-    ll_list = []
-    current = ll.head
-    while current != None:
-        ll_list.append(current.data)
-        current = current.next
+    # 연결리스트 뒤집기
+    lk = ll.head
+    reversed_ll = LinkedList()
+    while lk != None:
+        reversed_ll.insert_first(lk.data)
+        lk = lk.next
 
     # 팰린드롬인지 확인
-    for i in range(len(ll_list)):
-        if ll_list[i] != ll_list[len(ll_list) - 1 - i]:
+    # ll vs reverser_ll
+    current1 = ll.head
+    current2 = reversed_ll.head
+    while current1 != None:
+        # 하나라도 다르면 False
+        if current1.data != current2.data:
             return 0
-        return 1
+        current1 = current1.next
+        current2 = current2.next
+    # 모두 같으면 True
+    return 1
 
 if __name__ == '__main__':
     # Write your test cases here
@@ -25,6 +32,7 @@ if __name__ == '__main__':
     ll.insert_first(1)
     ll.insert_last(2)
     ll.insert_last(3)
+    # 1->2->3->
 
     ll_2 = LinkedList()
     ll_2.insert_first(3)
@@ -32,6 +40,7 @@ if __name__ == '__main__':
     ll_2.insert_last(3)
     ll_2.insert_last(5)
     ll_2.insert_first(5) 
+    # 5->3->9->3->5->
 
     testcase1 = ll
     testcase2 = ll_2
