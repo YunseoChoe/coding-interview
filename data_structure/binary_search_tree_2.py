7d957caf9974a59f45f8e1a1106cdd1c4c6770bb
choeyunseo <choeyunseo4060@naver.com>
2024-02-21 20:08:03 +0900

    Add binary_search_tree_2, 3

--- data_structure/binary_search_tree_2.py
+++ data_structure/binary_search_tree_2.py
@@ -1,75 +1,98 @@
 class Node:
     def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        self.root: Node = None 
+        self.value: int = value
+        self.left: Node = None
+        self.right: Node = None
 
-    def search(self, value):
-        # 트리가 비어있는 경우
-        if self.root == None:
-            print("트리가 비어있습니다.")
-            return None
-        # 트리가 비어있지 않은 경우
-        cur_node = self.root
-        while True:
-            # cur_node.value < value
-            if cur_node.value < value:
-                cur_node = cur_node.right
-            # cur_node.value > value
-            elif cur_node.value > value:
-                cur_node = cur_node.left
-            # cur_node.value == value
-            elif cur_node.value == value:
-                return value
-            else:
-                print("vatlue가 트리 안에 없습니다.")
-                break
+class BST:
+    def __init__(self):
+        self.root: Node = None
+    
+    def search(self, node: Node, value: int) -> bool:
+        if node == None:
+            return False
+        if node.value < value:
+            return self.search(node.right, value)
+        elif node.value > value:
+            return self.search(node.left, value)
+        else:
+            return True
 
-    def insert(self, value):
-        # 트리가 비어있다면
-        if self.root == None:
-            self.root = Node(value)
+    def insert(self, value: int):
+        self.root = self._insert(self.root, value)
+
+    def _insert(self, node: Node, value: int) -> Node:
+        # node가 비었을 때
+        if node == None:
+            node = Node(value)
+        elif node.value < value:
+            node.right = self._insert(node.right, value)
+        elif node.value > value:
+            node.left = self._insert(node.left, value)
+        return node
+    
+    def delete(self, value: int):
+        self.root = self._delete(self.root, value)
+
+    def _delete(self, node: Node, value) -> Node:
+        if node == None:
+            print("값이 없습니다.")
+        if node.value < value:
+            node.right = self._delete(node.right, value)
+        elif node.value > value:
+            node.left = self._delete(node.left, value)
+        else:
+            # 자식이 없을 때
+            if node.right == None and node.left == None:
+                return None
+            # 자식이 1개 있을 때 (오른쪽 자식만 있음)
+            elif node.right != None and node.left == None:
+                return node.right
+            # 자식이 1개 있을 때 (왼쪽 자식만 있음)
+            elif node.right == None and node.left != None:
+                return node.left
+            # 자식이 2개 있을 때
+            tmp = node.right
+            while tmp.left != None:
+                tmp = tmp.left
+            node.value = tmp.value
+            node.right = self._delete(node.right, tmp.value)
+        return node
+    
+    def inorder(self, node: Node):
+        if node == None:
             return
-        # 트리가 비어있지 않다면
-        cur_node = self.root
-        while True:
-            # cur_node.value < value
-            if cur_node.value < value:
-                if cur_node.right != None:
-                    cur_node = cur_node.right
-                else:
-                    break
-            # cur_node.value > value
-            elif cur_node.value > value:
-                if cur_node.left != None:
-                    cur_node = cur_node.left
-                else:
-                    break
-            # cur_node.value == value
-            else:
-                print("중복 발생...")
-                break
-            
-        if cur_node.value < value:
-            cur_node.right = Node(value)
-        elif cur_node.value > value:
-            cur_node.left = Node(value)
-
-
-    def delete(self, value):
-        # 트리가 비어있는 경우
-        if self.root == None:
-            print("트리가 비어있습니다.")
-        # 트리가 비어있지 않은 경우
-        
-
-    def inorder(self):
-        pass
+        else:
+            self.inorder(node.left)
+            print(node.value, end = "->")
+            self.inorder(node.right)
 
 
 
+bst = BST()
+bst.insert(5)
+bst.insert(3)
+bst.insert(2)
+bst.insert(8)
+bst.insert(9)
 
+bst.inorder(bst.root)
+# print(bst.root.value) # 5
+# bst.inorder(bst.root)
+print()
+# bst.root = bst.delete(bst.root, 5)
+# bst.root = bst.delete(bst.root, 3)
+# bst.root = bst.delete(bst.root, 8)
+bst.delete(5)
+bst.delete(3)
+bst.delete(8)
+
+bst.inorder(bst.root)
+
+
+# bst.search(bst.root, 0)
+# bst.search(bst.root, 10)
+# bst.search(bst.root, 3)
 
 
 
---
+++ data_structure/binary_search_tree_3.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, value: int): 
+        self.value = value
+        self.left: Node = None
+        self.right: Node = None
+
+    def search(self, value: int) -> bool:
+        if self.value < value: 
+            if self.right == None:
+                return False
+            else:
+                return self.right.search(value)
+        elif self.value > value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.search(value)
+        else:
+            return True # 있으면 True
+
+    def insert(self, value): #재귀함수
+        if self.value < value:
+            if self.right != None:
+                self.right.insert(value)
+            else:
+                self.right = Node(value)
+        elif self.value > value:
+            if self.left != None:
+                self.left.insert(value)
+            else:
+                self.left = Node(value)
+        else:
+            print("같은 노드가 있습니다.")
+
+    def delete(self, value):
+        if self.value < value:
+            self.right = self.right.delete(value)
+        elif self.value > value:
+            self.left = self.left.delete(value)
+        # self.value == vlaue
+        else:
+            # 자식 노드가 0개인 경우
+            if self.left == None and self.right == None:
+                return None
+            # 자식 노드가 1개인 경우
+            # 왼쪽 자식
+            elif self.left != None and self.right == None:
+                return self.left
+            # 오른쪽 자식
+            elif self.left == None and self.right != None:
+                return self.right
+            # 자식 노드가 2개인 경우
+            else:
+                # tmp = Node(self.right.get_min_node())
+                tmp = self.right.get_min_node()
+                # print(f'tmp:{tmp}')
+                # print(type(tmp))
+                self.value = tmp
+                # tmp 삭제
+                self.right = self.right.delete(tmp)
+
+        return self
+
+    # 최솟값을 가지고 있는 노드 찾기
+    def get_min_node(self):
+        if self.left != None:
+            return self.left.get_min_node() # 재귀 호출된 함수의 반환 값을 상위 호출자에게 반환
+        else:
+            return self.value
+        
+    # 중위순회
+    def inorder(self):
+        if self.left != None:
+            self.left.inorder()
+        print(self.value, end = "->")
+        if self.right != None:
+            self.right.inorder()
+
+root = Node(10)
+root.insert(13)
+root.insert(0)
+root.insert(14)
+root.insert(1)
+root.insert(11)
+
+root.inorder()
+print()
+
+root.delete(10)
+root.inorder()
